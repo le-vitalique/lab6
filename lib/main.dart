2e82ddd73eca3665cd86933238f66bffc9d3387e
Виталий Гаджиян <vita1ka@inbox.ru>
2024-10-15 19:13:59 +0400

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -120,10 +120,13 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        //backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -146,9 +149,12 @@ class _MyHomePageState extends State<MyHomePage> {
           children: <Widget>[
             Padding(
                 padding: const EdgeInsets.all(16.0),
-                child:
-                    isLoading ? const CircularProgressIndicator() : Text(text)),
-            TextButton(onPressed: isLoading ? () {} : getHttpData, child: const Text('HTTP')),
+                child: isLoading
+                    ? const CircularProgressIndicator(color: Colors.blueGrey)
+                    : Text(text)),
+            TextButton(
+                onPressed: isLoading ? () {} : getHttpData,
+                child: const Text('HTTP')),
             TextButton(onPressed: () {}, child: const Text('DIO')),
             TextButton(onPressed: () {}, child: const Text('ERROR')),
           ],
