09f628d64cc6e315c0570033bed3491f8711a928
Виталий Гаджиян <vita1ka@inbox.ru>
2024-10-17 18:38:22 +0400

    removed getDataHttp from initState

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,6 @@ class _MyHomePageState extends State<MyHomePage> {
     _dio.options.baseUrl = 'https://dummyjson.com';
     _dio.options.connectTimeout = const Duration(seconds: 5);
     _dio.options.receiveTimeout = const Duration(seconds: 3);
-    getDataHttp();
   }
 
   getDataHttp() async {
